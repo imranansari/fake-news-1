946897ee562cc8bece0744f22fe2e12b255bd663
Mihail Eric <mihail911@gmail.com>
2021-01-15 08:35:23 -0800

    add server file

---
+++ fake_news/server/main.py
@@ -0,0 +1,51 @@
+import os
+from typing import List
+
+import numpy as np
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseSettings
+from pydantic.main import BaseModel
+
+from fake_news.model.tree_based import RandomForestModel
+from fake_news.utils.features import TreeFeaturizer
+from fake_news.utils.features import construct_datapoint
+
+
+class Settings(BaseSettings):
+    model_dir: str
+
+
+app = FastAPI()
+settings = Settings()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load featurizer and model
+featurizer = TreeFeaturizer(os.path.join(settings.model_dir, "featurizer.pkl"))
+model = RandomForestModel(os.path.join(settings.model_dir, "model.pkl"))
+
+
+class Statement(BaseModel):
+    text: str
+
+
+class Prediction(BaseModel):
+    label: float
+    probs: List[float]
+
+
+@app.post("/api/predict-fakeness", response_model=Prediction)
+def predict_fakeness(statement: Statement):
+    datapoint = construct_datapoint(statement.text)
+    features = featurizer.featurize([datapoint])
+    probs = model.predict(features)
+    label = np.argmax(probs, axis=1)
+    return Prediction(label=label[0], probs=list(probs[0]))
